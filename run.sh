32c0e5a55d04e46380e4011b9d35fcf4f02c1790
WeiFoo <wfu@ncsu.edu>
2016-02-21 12:29:01 -0500

    fix

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ rm err/*
 ##### this is only for testing on HPC, tag level
 foreach goal (precision f1 auc)
   foreach r (true false)
-    bsub -W 1200 -o ./out/${goal}_${r}.out.%J -e ./err/${goal}_${r}.err.%J /share3/wfu/miniconda/bin/python2.7 run.py run goal r
+    bsub -W 1200 -o ./out/${goal}_${r}.out.%J -e ./err/${goal}_${r}.err.%J /share3/wfu/miniconda/bin/python2.7 run.py run $goal $r
   end
 end
